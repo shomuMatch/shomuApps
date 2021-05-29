27083322e273e6d0c2c56cc93f95c3a404f7c32f
shomuMatch <>
2021-05-29 17:45:39 +0900

    update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,12 +12,3 @@ resource "aws_s3_bucket_policy" "frontend_s3_policy" {
   bucket = aws_s3_bucket.frontend.id
   policy = data.aws_iam_policy_document.frontend_bucket_policy.json
 }
-
-# resource "aws_s3_bucket_object" "frontend_files" {
-#   for_each     = fileset("frontend/video-meeting/build/", "**")
-#   bucket       = aws_s3_bucket.frontend.id
-#   key          = each.value
-#   source       = "frontend/video-meeting/build/${each.value}"
-#   content_type = "text/html"
-#   etag         = filemd5("frontend/video-meeting/build/${each.value}")
-# }
